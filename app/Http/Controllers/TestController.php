5430d440b0ce775a92cd8f51dee69deb9b7ea16c
AmadulHaque <92516695+AmadulHaque@users.noreply.github.com>
2025-07-31 17:57:25 +0600

    Refactor TestController's test method to display user attendance with names; remove debug dump

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -30,10 +30,39 @@ class TestController extends Controller
 
         $zk->connect();
 
-        dd( 
-            $zk->getAttendance(),
-            $zk->getUser(),
-        );
+        $users = $zk->getUser();
+        $attendance = $zk->getAttendance(); 
+
+        $zk->disconnect();
+
+        // echo "Users List: ". count($users)." <br> ";
+        // foreach ($users as $user) {
+        //     echo "ID: " . $user[0] . "<br>";
+        //     echo "Name: " . $user[1] . "<br>";
+        //     echo "Card Number: " . $user[2] . "<br>";
+        //     echo "Privilege: " . $user[3] . "<br>";
+        //     echo "Password: " . $user[4] . "<br>";
+        //     echo "------------------------<br>";
+        // }
+
+
+        $userMap = [];
+        foreach ($users as $user) {
+            $userMap[$user[0]] = $user[1]; // $user[0] = ID, $user[1] = Name
+        }
+
+        // Show attendance list with user names
+        echo "<br>Attendance List: " . count($attendance) . " <br>";
+        foreach ($attendance as $att) {
+            $userId = $att[1];
+            $userName = isset($userMap[$userId]) ? $userMap[$userId] : 'Unknown';
+
+            echo "ID: " . $att[0] . "<br>";
+            echo "User ID: " . $userId . "<br>";
+            echo "User Name: " . $userName . "<br>";
+            echo "Date Time: " . $att[3] . "<br>";
+            echo "------------------------<br>";
+        }
 
 
 
